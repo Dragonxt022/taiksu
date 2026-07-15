d72b8e20437a4ce03fafd98ec96b2b2b551bb63d
Taiksu <gestao@taiksu.com.br>
2026-07-15 16:26:10 -0400

    feat: adiciona suporte a layouts EJS e configura variáveis de sessão padrão

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var expressLayouts = require('express-ejs-layouts');
 var logger = require('morgan');
 var createError = require('http-errors');
 var db = require('./models');
@@ -19,6 +20,8 @@ var app = express();
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
 
 // Configura o CORS
 var corsAuth = {
@@ -44,6 +47,20 @@ app.use(session({
   },
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Defaults para as variáveis de sessão usadas no layout/partials (header, userBar),
+// evitando ReferenceError quando o EJS renderiza fora do fluxo autenticado (ex: erro em /api).
+app.use((req, res, next) => {
+  res.locals.userFoto = null;
+  res.locals.userNome = null;
+  res.locals.userGrupo = null;
+  res.locals.userCidade = null;
+  res.locals.user_id = null;
+  res.locals.userToken = null;
+  res.locals.unidade_id = null;
+  res.locals.userPin = null;
+  next();
+});
 
 // Test DB connection
 db.sequelize.authenticate()
